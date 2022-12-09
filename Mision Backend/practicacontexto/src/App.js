e45c0aca2e865f37f6aee86785464ba674c2621a
Julian Millar <julianjmillar@gmail.com>
2022-12-09 08:52:58 -0300

    Nueva practica CTX

---
+++ Mision Backend/practicacontexto/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+import Username from './components/Username';
+
+export const UserContext = React.createContext();
+
+function App() {
+  return (
+    <UserContext.Provider value="Jesus">
+      <Username/>
+    </UserContext.Provider>
+  );
+}
+
+function User () {
+  const value = React.useContext(UserContext);
+  return <h1>{value}</h1>
+
+  /*return (
+    <UserContext.Consumer>
+      {value => <h1>{value}</h1>}
+    </UserContext.Consumer>
+  )*/
+}
+
+export default App;
