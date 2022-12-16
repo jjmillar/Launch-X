2dcc2489df25e3a420ddcd6376983ccdf55cf41b
Julian Millar <julianjmillar@gmail.com>
2022-12-16 08:54:05 -0300

    avance final

--- Mision Backend/node and express/index.js
+++ Mision Backend/node and express/index.js
@@ -1,9 +1,29 @@
 let express = require('express');
 let app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Read
 app.get('/', function(req, res) {
     res.send("Hola mundo");
 });
+
+// Create
+app.post('/', function(req, res) {
+    res.send(`Te llamas ${req.body.name}`);
+});
+
+// Update
+app.put('/', function(req, res) {
+    res.send(`Has actualizado el registro numero ${req.body.id}`)
+});
+
+// Delete
+app.delete('/users', function (req, res) {
+    res.send(`Has eliminado el registro: ${req.query.user}`)
+});
+
 
 let server = app.listen(8080, function() {
     let host = server.address().address;
