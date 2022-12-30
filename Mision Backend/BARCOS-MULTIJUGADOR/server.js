be4340e329f667df8f883a5d22de68e0aba140bd
Julian Millar <julianjmillar@gmail.com>
2022-12-30 08:51:37 -0300

    Avance

--- Mision Backend/BARCOS-MULTIJUGADOR/server.js
+++ Mision Backend/BARCOS-MULTIJUGADOR/server.js
@@ -10,3 +10,49 @@ const io = socket(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 server.listen(PORT, () => console.log("El servidor esta corriendo"));
+
+const connections = [null, null];
+
+io.on('connection', socket => {
+    let playerIndex = -1;
+    for (const i in connections) {
+        if (connections[i] === null) {
+            playerIndex = i;
+            break;
+        }
+    };
+
+    socket.emit("player-number", playerIndex);
+
+    console.log(`Jugador ${playerIndex} se ha conectado`);
+
+    if (playerIndex === -1) return;
+    connections[playerIndex] = false;
+
+    socket.broadcast.emit('player-connection', playerIndex);
+
+    socket.on('disconnect', () => {
+        console.log(`Jugador ${playerIndex} se ha desconectado`);
+        connections[playerIndex] = null;
+        socket.broadcast.emit('player-connection', playerIndex);
+    });
+
+    socket.on('player-ready', () => {
+        socket.broadcast.emit('enemy-ready', playerIndex);
+        connections[playerIndex] = true;
+    });
+
+    socket.on('check-players', () => {
+        const players = [];
+        for (const i in connections) {
+            connections[i] === null ? 
+                players.push({connected: false, ready: false}) :
+                players.push({connected: true, ready: connections[i]});
+        }
+        socket.emit('check-players', players)
+    });
+
+    socket.on('fire', id =>)
+
+});
+
