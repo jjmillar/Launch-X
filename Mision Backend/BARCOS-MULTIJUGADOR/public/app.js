8cf466f97b00c8b00cca769171c19318b9a63981
Julian Millar <julianjmillar@gmail.com>
2023-01-05 08:54:03 -0300

    avance

--- Mision Backend/BARCOS-MULTIJUGADOR/public/app.js
+++ Mision Backend/BARCOS-MULTIJUGADOR/public/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const userGrid = document.querySelector('.grid-user');
-    const cpurGrid = document.querySelector('.grid-cpu');
+    const cpuGrid = document.querySelector('.grid-cpu');
     const displayGrid = document.querySelector('.grid-display');
 
     const ships = document.querySelector('.ship');
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const width = 10;
     const userSquares = [];
-    const scpuSquares = [];
+    const cpuSquares = [];
 
     let isGameOver = false;
     let ready = false;
@@ -86,6 +86,102 @@ document.addEventListener('DOMContentLoaded', () => {
             grid.appendChild(square);
             squares.push(square);
         }
+    };
+
+    function startSingleplayer() {
+
+    };
+
+    function generar(ship) {
+        let randomDirection = Math.floor(Math.random() * ship.directions.length);
+        let current = ship.directions [randomDirection];
+        if (randomDirection === 0) directions = 1;
+        if (randomDirection === 1) directions = 10;
+
+        const isTaken = current.some(index => cpuSquares[index].classList.contains('taken'));
+
+        const isAtRightEdge = current.some(index => (index) % width === width - 1);
+        const isAtLightEdge = current.some(index => (index) % width === width - 0);
+
+    };
+
+    function startMultiplayer () {
+        const socket = io();
+
+        socket.on('player-number', num => {
+            if (num === -1) {
+                infoDisplay.innerHTML = "Servidor lleno";
+            } else {
+                playerNum = parseInt(num);
+                if(playerNum === 1) currentPlayer = "enemy";
+
+                console.log(playerNum);
+
+                socket.emit('check-players');
+            }
+        });
+
+        socket.on('player-connection', num => {
+            console.log(`Jugador ${num} se ha conectado`);
+            playerisConnected(num);
+        });
+
+        socket.on('enemy-ready', num => {
+            enemyReady = true;
+            playerReady(num);
+            if (ready) {
+                //playGameMulti(socket);
+                setupButtons.style.display = 'none';
+            }
+        });
+
+        socket.on('check-players', players => {
+            players.forEach((p, i) => {
+                if (p.connected) playerisConnected(i);
+                if (p.ready) {
+                    playerReady(i);
+                    if (i !== playerReady) enemyReady = true;
+                }
+            });
+        });
+
+        socket.on('timeout', () => {
+            infoDisplay.innerHTML = "Te has pasado del tiempo de espera";
+        });
+
+        startNutton.addEventListener('click', () => {
+            if(allShipsPlaced) {
+                console.log("Comienza el juego");
+                // playGameMulti(socket);
+            } else {
+                infoDisplay.innetHTML = "Por favor, coloca TODOS los barcos";
+            };
+        });
+
+        cpuSquares.forEach(square => {
+            square.addEventListener('click', () => {
+                if (currentPlayer === 'user' && ready && enemyReady) {
+                    shotsFired = square.dataset.id;
+                    socket.emit('fire', shotsFired)
+                }
+            });
+        });
+
+        socket.on
+
+    };
+
+    function playerReady(num) {
+        let player = `.p${parseInt(num) + 1}`;
+        document.querySelector(`${player} .ready`).classList.toggle('active');
+    };
+
+    function playerisConnected(num) {
+        let player = `.p${parseInt(num) + 1}`;
+        document.querySelector(`${player} .connected`).classList.toggle('active');
+
+        if (parseInt(num) === playerNum) 
+            document.querySelector(player).style.fontWeight = 'bold';
     };
 
 });
