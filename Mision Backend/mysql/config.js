d386db0156ba1f86e7905d7808f629beab3d8e46
Julian Millar <julianjmillar@gmail.com>
2023-01-20 08:55:00 -0300

    avance

---
+++ Mision Backend/mysql/config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: process.env.HOSTNAME,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+    },
+    listPerPage: 10
+};
+
+module.exports = config;
