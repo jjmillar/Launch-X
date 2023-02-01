051f76eb8fc0d6bad29b5d893f0cd1963eccf349
Julian Millar <julianjmillar@gmail.com>
2023-02-01 08:49:37 -0300

    initial commit

---
+++ Mision Backend/typescript/src/annotations.ts
@@ -0,0 +1,13 @@
+// Genera numeros entre el 500 y el 1000
+const RANDOM_TIME = 
+    Math.round(Math.random() * 500) + 500;
+
+let startTime = new Date();
+// let endTime: any
+// Aqui permitimos meterle cualquier tipo de dato a nuestra variable
+let endTime
+
+setTimeout (() => {
+    endTime = 0
+    endTime = new Date()
+}, RANDOM_TIME)
---
+++ Mision Backend/typescript/src/variables.ts
@@ -0,0 +1,6 @@
+// En typescript se habilita el tipado fuerte o estatico, es decir, que un dato que empieza como number, no puede ser cambiado de ese tipo de dato
+
+let edad = 18
+// Esto genera un error
+// edad = "dieciocho anos";
+const edad2 = 19;
